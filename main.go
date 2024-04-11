ed51e7b3e807e7eb812d52ef30275032d21fbc55
windsyu <windsyu2811@gmail.com>
2024-04-11 19:12:16 +0800

    UPdateDeploy

--- main.go
+++ main.go
@@ -1,72 +1,132 @@
 package main
 
 import (
-	"bytes"
 	"context"
-	"html/template"
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/util/yaml"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+	appsresv1 "k8s.io/client-go/kubernetes/typed/apps/v1"
 	"k8s.io/client-go/tools/clientcmd"
-	"log"
+	"k8s.io/client-go/util/retry"
+	"k8s.io/klog/v2"
 )
 
-type PodSpec struct {
-	Name          string `json:"name"`
-	Image         string `json:"image"`
-	Namespace     string `json:"namespace"`
-	ContainerName string `json:"container_name"`
-}
-
 func main() {
+	//1. create configuration
 	config, err := clientcmd.BuildConfigFromFlags("", "C:/Users/27380/.kube/config")
 	if err != nil {
 		panic(err)
 	}
-
+	//2.create clientset
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		panic(err)
 	}
 
-	spec := PodSpec{
-		Name:          "nginx-pod-demo",
-		Image:         "nginx",
-		Namespace:     "default",
-		ContainerName: "nginx",
-	}
-	var pod corev1.Pod
-	tmpl, err := ParseTemplate("./template/nginx.yaml", &spec)
-	if err != nil {
-		panic(err)
-	}
-	//将tmpl字节流按yaml解析放入pod变量中
-	err = yaml.Unmarshal(tmpl, &pod)
-	if err != nil {
-		panic(err)
-	}
+	//3.create deployment
+	deployClient := clientset.AppsV1().Deployments(corev1.NamespaceDefault)
+	CreateDeploy(deployClient)
 
-	//4.创建pod
-	_, err = clientset.CoreV1().Pods("default").Create(context.Background(), &pod, metav1.CreateOptions{})
-	if err != nil {
-		log.Printf("create pod error:%v\n", err)
-		return
-	}
-	log.Printf("create pod success")
+	//4.update strategy
+	UpdateDeploy(deployClient)
+	prompt()
+
+	//5.view deployment
+	ListDeploy(deployClient)
+
+	prompt()
+
+	//6.Delete Deployment
+	DeleteDeploy(deployClient)
 }
 
-func ParseTemplate(name string, item *PodSpec) ([]byte, error) {
-	tmpl, err := template.ParseFiles(name)
-	if err != nil {
-		return nil, err
-	}
-	buf := bytes.Buffer{}
-	//Execute write parsed template to a buf（tmpl是template文件，item是动态装载的PodSpec项，两者会被对应装配后写入buf）
-	err = tmpl.Execute(&buf, item)
-	if err != nil {
-		return nil, err
+func CreateDeploy(client appsresv1.DeploymentInterface) {
+	klog.Info("CreateDeploy.........")
+	replicas := int32(2)
+	deploy := appsv1.Deployment{
+		TypeMeta: v1.TypeMeta{
+			Kind:       "Deployment",
+			APIVersion: "apps/v1",
+		},
+		ObjectMeta: v1.ObjectMeta{
+			Name:      "deploy-nginx-demo",
+			Namespace: corev1.NamespaceDefault,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &replicas,
+			Selector: &v1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": "nginx",
+				},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: v1.ObjectMeta{
+					Name: "nginx",
+					Labels: map[string]string{
+						"app": "nginx",
+					},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  "web",
+							Image: "nginx:1.12",
+							Ports: []corev1.ContainerPort{
+								{
+									Protocol:      corev1.ProtocolTCP,
+									ContainerPort: 80,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
 	}
 
-	return buf.Bytes(), nil
+	dep, err := client.Create(context.Background(), &deploy, v1.CreateOptions{})
+	if err != nil {
+		klog.Errorf("create deployment error:%v", err)
+		return
+	}
+	klog.Infof("create deployment success,name:%s", dep.Name)
+}
+
+func UpdateDeploy(client appsresv1.DeploymentInterface) {
+	klog.Info("Updating Deploy..................")
+	//多个客户端对同一个资源操作时会发生错误，所以使用RetryOnConflict来重试
+	err := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+		//查询要更新的deploy
+		deploy, err := client.Get(context.Background(), "deploy-nginx-demo", v1.GetOptions{})
+		if err != nil {
+			klog.Errorf("can't get deployment, err:%v", err)
+			return nil
+		}
+		replicas := int32(1)
+		deploy.Spec.Replicas = &replicas
+		deploy.Spec.Template.Spec.Containers[0].Image = "nginx:1.13"
+
+		_, err = client.Update(context.Background(), deploy, v1.UpdateOptions{})
+		if err != nil {
+			klog.Errorf("update deployment error,err:%v", err)
+		}
+		return err
+	})
+
+	if err != nil {
+
+	}
+}
+
+func prompt() {
+
+}
+
+func ListDeploy(client appsresv1.DeploymentInterface) {
+
+}
+
+func DeleteDeploy(client appsresv1.DeploymentInterface) {
+
 }
---
+++ template/nginx_template.go
@@ -0,0 +1,72 @@
+package template
+
+import (
+	"bytes"
+	"context"
+	"html/template"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/yaml"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"log"
+)
+
+type PodSpec struct {
+	Name          string `json:"name"`
+	Image         string `json:"image"`
+	Namespace     string `json:"namespace"`
+	ContainerName string `json:"container_name"`
+}
+
+func main() {
+	config, err := clientcmd.BuildConfigFromFlags("", "C:/Users/27380/.kube/config")
+	if err != nil {
+		panic(err)
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+
+	spec := PodSpec{
+		Name:          "nginx-pod-demo",
+		Image:         "nginx",
+		Namespace:     "default",
+		ContainerName: "nginx",
+	}
+	var pod corev1.Pod
+	tmpl, err := ParseTemplate("./nginx.yaml", &spec)
+	if err != nil {
+		panic(err)
+	}
+	//将tmpl字节流按yaml解析放入pod变量中
+	err = yaml.Unmarshal(tmpl, &pod)
+	if err != nil {
+		panic(err)
+	}
+
+	//4.创建pod
+	_, err = clientset.CoreV1().Pods("default").Create(context.Background(), &pod, metav1.CreateOptions{})
+	if err != nil {
+		log.Printf("create pod error:%v\n", err)
+		return
+	}
+	log.Printf("create pod success")
+}
+
+func ParseTemplate(name string, item *PodSpec) ([]byte, error) {
+	tmpl, err := template.ParseFiles(name)
+	if err != nil {
+		return nil, err
+	}
+	buf := bytes.Buffer{}
+	//Execute write parsed template to a buf（tmpl是template文件，item是动态装载的PodSpec项，两者会被对应装配后写入buf）
+	err = tmpl.Execute(&buf, item)
+	if err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+}
